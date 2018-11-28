cc199f76435fdd6c9b6aebe8348d1b0493855878
Adrián Navarro <adnaga94@gmail.com>
2018-11-28 15:20:52 +0100

    Exercise 059 - Comparing ages

---
+++ exercises/059-Age.py
@@ -0,0 +1,12 @@
+# Comparing ages
+# Adrián Navarro Gabino
+
+p1 = int(input("Age of first person: "))
+p2 = int(input("Age of second person: "))
+
+if p1 < p2:
+    print("The first person is older than the second one")
+elif p1 > p2:
+    print("The second person is older than the first one")
+else:
+    print("Both are the same age")
