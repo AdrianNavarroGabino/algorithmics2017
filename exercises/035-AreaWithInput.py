0808e5d36fe6c224dfda3f9fa553ea03e496c35b
Adrián Navarro <adnaga94@gmail.com>
2018-11-28 12:29:37 +0100

    Exercise 035 - Perimeter and area of a square with an input

---
+++ exercises/035-AreaWithInput.py
@@ -0,0 +1,10 @@
+# Perimeter and area with an input
+# Adrián Navarro Gabino
+
+side = float(input("Introduce el lado del cuadrado: "))
+
+perimeter = side * 4
+area = side**2
+
+print(perimeter)
+print(area)
