8c3a2ced3fcd9ac9230b46a48e99be6e05309dbc
Adrián Navarro <adnaga94@gmail.com>
2018-11-28 13:02:36 +0100

    Exercise 037 - Area of a triangle with and input

---
+++ exercises/037-TriangleInput.py
@@ -0,0 +1,9 @@
+# Area of a triangle with and input
+# Adrián Navarro Gabino
+
+base = float(input("Enter the base: "))
+height = float(input("Enter the height: "))
+
+area = base*height/2
+
+print(area)
