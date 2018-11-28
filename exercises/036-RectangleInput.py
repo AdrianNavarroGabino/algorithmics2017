682a35c11f12499bd1938a0cff19d1c700a0302a
Adrián Navarro <adnaga94@gmail.com>
2018-11-28 12:33:24 +0100

    Exercise 036 - Perimeter and area of a rectangle with and input

---
+++ exercises/036-RectangleInput.py
@@ -0,0 +1,11 @@
+# Perimeter and area of a rectangle with and input
+# Adrián Navarro Gabino
+
+side1 = float(input("Enter side #1: "))
+side2 = float(input("Enter side #2: "))
+
+perimeter = side1*2 + side2*2
+area = side1 * side2
+
+print(perimeter)
+print(area)
